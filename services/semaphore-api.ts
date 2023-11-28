f0de91b7384011664b6038108896d2d6956a2870
mlester0806 <114319938+mlester0806@users.noreply.github.com>
2023-11-29 05:55:05 +0800

    Update semaphore-api.ts

--- services/semaphore-api.ts
+++ services/semaphore-api.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const semaphoreApi = createApi({
   reducerPath: 'semaphoreApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://localhost:4000/',
+    baseUrl: process.env.NEXT_PUBLIC_API_URL,
     credentials: 'include',
   }),
   tagTypes: ['Messages'],
