6af20999da254c37f9dfcd4cd70ba6ba18897659
Mark Lester Acak <mister6virgo@gmail.com>
2023-11-29 06:35:50 +0800

    update latest products and categories in homepage

--- components/overview-categories/CategoriesMain.tsx
+++ components/overview-categories/CategoriesMain.tsx
@@ -1,17 +1,23 @@
-'use client';
+"use client";
 
-import { useGetCategoriesQuery } from '@/services/crud-category';
-import CategoryCard from './CategoryCard';
-import SkeletonCategories from './SkeletonCategories';
+import { useGetCategoriesQuery } from "@/services/crud-category";
+import CategoryCard from "./CategoryCard";
+import SkeletonCategories from "./SkeletonCategories";
 
 const CategoriesMain = () => {
   const { data: categoriesData } = useGetCategoriesQuery();
 
   return (
-    <div className='grid grid-cols-1 gap-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 mt-6'>
-      {!categoriesData || categoriesData?.length === 0 ? (
-        <SkeletonCategories />
-      ) : null}
+    <div className="grid grid-cols-1 gap-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 mt-6">
+      {!categoriesData && <SkeletonCategories />}
+
+      {categoriesData && categoriesData?.length === 0 && (
+        <div className="py-8 my-8 bg-gray-100">
+          <p className="text-center w-full text-gray-900 font-semibold">
+            No Categories Found..
+          </p>
+        </div>
+      )}
 
       {categoriesData?.map((category) => (
         <CategoryCard key={category.id} category={category} />
--- components/product/LatestProduct.tsx
+++ components/product/LatestProduct.tsx
@@ -1,13 +1,13 @@
-'use client';
+"use client";
 
-import { useDispatch, useSelector } from 'react-redux';
-import { useGetLatestProductsQuery } from '@/services/crud-product';
-import Modal from '../Modal';
-import ProductCard from './ProductCard';
-import SkeletonProductCards from './SkeletonProductCards';
-import { useRouter } from 'next/navigation';
-import { closeModal } from '@/slices/cartSlice';
-import Link from 'next/link';
+import { useDispatch, useSelector } from "react-redux";
+import { useGetLatestProductsQuery } from "@/services/crud-product";
+import Modal from "../Modal";
+import ProductCard from "./ProductCard";
+import SkeletonProductCards from "./SkeletonProductCards";
+import { useRouter } from "next/navigation";
+import { closeModal } from "@/slices/cartSlice";
+import Link from "next/link";
 
 const LatestProduct = () => {
   const { data: productsData } = useGetLatestProductsQuery();
@@ -21,11 +21,11 @@ const LatestProduct = () => {
   };
 
   const runLoginNav = () => {
-    router.push('/customer/login');
+    router.push("/customer/login");
   };
 
   const runSignUpNav = () => {
-    router.push('/customer/signup');
+    router.push("/customer/signup");
   };
 
   return (
@@ -33,41 +33,47 @@ const LatestProduct = () => {
       <Modal
         title={`Oops... It look's like you're not logged in yet.`}
         description={`You need to login to your account first. If you don't have an account yet, please don't hesitate to register.`}
-        status='failed'
+        status="failed"
         open={Boolean(!isLoggedIn && isAddedProduct)}
-        leftBtnTitle='Login'
-        rightBtnTitle='Create Account'
+        leftBtnTitle="Login"
+        rightBtnTitle="Create Account"
         closeModal={runCloseModal}
         leftBtnFunc={runLoginNav}
         rightBtnFunc={runSignUpNav}
       />
-      <section aria-labelledby='trending-heading' className='bg-gray-50'>
-        <div className='py-16 sm:py-24 lg:mx-auto lg:max-w-7xl lg:px-8 lg:py-32'>
-          <div className='flex items-center justify-between px-4 sm:px-6 lg:px-0'>
+      <section aria-labelledby="trending-heading" className="bg-gray-50">
+        <div className="py-16 sm:py-24 lg:mx-auto lg:max-w-7xl lg:px-8 lg:py-32">
+          <div className="flex items-center justify-between px-4 sm:px-6 lg:px-0">
             <h2
-              id='trending-heading'
-              className='text-2xl font-bold tracking-tight text-gray-900'
+              id="trending-heading"
+              className="text-2xl font-bold tracking-tight text-gray-900"
             >
               Latest Products
             </h2>
             <Link
-              href='/products'
-              className='hidden text-sm font-semibold text-indigo-600 hover:text-indigo-500 sm:block'
+              href="/products"
+              className="hidden text-sm font-semibold text-indigo-600 hover:text-indigo-500 sm:block"
             >
               See everything
-              <span aria-hidden='true'> &rarr;</span>
+              <span aria-hidden="true"> &rarr;</span>
             </Link>
           </div>
 
-          <div className='relative'>
-            <div className='relative w-full overflow-x-auto'>
+          <div className="relative">
+            <div className="relative w-full overflow-x-auto">
               <ul
-                role='list'
-                className='mx-4 inline-flex space-x-8 sm:mx-6 lg:mx-0 lg:grid lg:grid-cols-4 lg:gap-x-8 lg:space-x-0'
+                role="list"
+                className="mx-4 inline-flex space-x-8 sm:mx-6 lg:mx-0 lg:grid lg:grid-cols-4 lg:gap-x-8 lg:space-x-0"
               >
-                {!productsData || productsData?.length === 0 ? (
-                  <SkeletonProductCards />
-                ) : null}
+                {!productsData && <SkeletonProductCards />}
+
+                {productsData?.length === 0 && (
+                  <div className="py-8 my-8 bg-gray-100">
+                    <p className="text-center w-full text-gray-900 font-semibold">
+                      No Latest Products Found..
+                    </p>
+                  </div>
+                )}
 
                 {productsData?.map((product) => (
                   <ProductCard key={product.id} product={product} />
@@ -76,13 +82,13 @@ const LatestProduct = () => {
             </div>
           </div>
 
-          <div className='mt-12 px-4 sm:hidden'>
+          <div className="mt-12 px-4 sm:hidden">
             <Link
-              href='/products'
-              className='text-sm font-semibold text-indigo-600 hover:text-indigo-500'
+              href="/products"
+              className="text-sm font-semibold text-indigo-600 hover:text-indigo-500"
             >
               See everything
-              <span aria-hidden='true'> &rarr;</span>
+              <span aria-hidden="true"> &rarr;</span>
             </Link>
           </div>
         </div>
