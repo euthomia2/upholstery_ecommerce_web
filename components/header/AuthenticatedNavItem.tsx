08443e53f27c616d6716cd7d6a51f3f4cbbd4fa3
mlester0806 <114319938+mlester0806@users.noreply.github.com>
2023-12-04 22:51:12 +0800

    Update AuthenticatedNavItem.tsx

--- components/header/AuthenticatedNavItem.tsx
+++ components/header/AuthenticatedNavItem.tsx
@@ -51,7 +51,7 @@ const AuthenticatedNavItem = () => {
   }
 
   return (
-    <Menu as="div" className="relative inline-block text-left">
+    <Menu as="div" className="relative inline-block text-left hidden lg:flex">
       <div>
         <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
           Hi, {user?.first_name} {user?.last_name}
